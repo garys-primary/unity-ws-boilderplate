0b85d3759b57c96964ce64d157bf752909125511
garys-primary <gary.sedletsky@gmail.com>
2023-01-12 18:20:39 -0800

    boilerplate commit

---
+++ Assets/Scripts/README.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class README : MonoBehaviour
+{
+/*
+
+install NativeWebSocket
+https://github.com/endel/NativeWebSocket/tree/575f41f0ec1325af385b0f08405dd1b2dc604764
+
+Unity > Window > Package Manager
+Click Add [+] > Package From Git URL
+Enter URL: https://github.com/endel/NativeWebSocket.git#upm
+
+Native WebSokets, author Endel Dreyer
+Version 1.1.4
+
+Using certificates
+https://medium.com/@hacj/unity-webgl-and-websockets-a-guide-42f3e8f0db34
+
+Realistic Flight physics
+https://www.youtube.com/watch?v=7vAHo2B1zLc
+
+Unity flight controller script 
+https://github.com/Translab/Unity-Fly-Control/blob/master/Control/CollisionDetect.cs
+
+
+
+*/
+}
